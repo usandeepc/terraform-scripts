8d82ee13aa5b04a37ebf08d93b8f1fce8d2a699e
Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2021-05-21 11:42:51 +0530

    Security Group Inbound changed

--- vpc.tf
+++ vpc.tf
@@ -197,6 +197,13 @@ resource "aws_security_group" "eks-cluster-sg" {
   description = "EKS Cluster Security Group"
   vpc_id      = aws_vpc.main.id
 
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port        = 0
     to_port          = 0
