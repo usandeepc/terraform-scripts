b0e451789e2f7e6c325e1b8a1637c48621f81f05
Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2021-05-21 19:35:12 +0530

    Added EKS and EKS NodeGroup Support

---
+++ eks-nodegroup.tf
@@ -0,0 +1,20 @@
+resource "aws_eks_node_group" "nodegroup" {
+  cluster_name    = aws_eks_cluster.aws_eks_cluster.name
+  node_group_name = "${var.tf_run_name}-eks-nodegroup"
+  node_role_arn   = aws_iam_role.eks_nodes_role.arn
+  subnet_ids      = [aws_subnet.pvt-subnet-01.id, aws_subnet.pvt-subnet-02.id,aws_subnet.pvt-subnet-03.id]
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 2
+    min_size     = 1
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
+  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
+  depends_on = [
+    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+  ]
+}
---
+++ eks.tf
@@ -0,0 +1,12 @@
+resource "aws_eks_cluster" "aws_eks_cluster" {
+  name     = "${var.tf_run_name}-eks-cluster"
+  role_arn = aws_iam_role.eks_cluster_role.arn
+
+  vpc_config {
+    subnet_ids = [aws_subnet.pvt-subnet-01.id, aws_subnet.pvt-subnet-02.id,aws_subnet.pvt-subnet-03.id,aws_subnet.pub-subnet-01.id,aws_subnet.pub-subnet-02.id,aws_subnet.pub-subnet-03.id]
+  }
+
+  tags = {
+    Name = "${var.tf_run_name}-eks-cluster"
+  }
+}
