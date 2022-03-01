51447c191bad111cd46f900b93a42f6b78798890
Uppala Sandeep Chowdary <sandeepchowdaryu@gmail.com>
2022-03-01 12:06:14 +0530

    Update eks.tf

--- eks.tf
+++ eks.tf
@@ -28,33 +28,4 @@ resource "aws_iam_openid_connect_provider" "cluster" {
   }
 }
 
-resource "aws_iam_policy" "eks_alb_policy" {
-  name        = "AWSLoadBalancerControllerIAMPolicy"
-  description = "Service account Policy for the ALB Ingress"
 
-  policy = file("iam-policy.json")
-}
-
-resource "aws_iam_role" "eks_alb_role" {
-  name = "${var.tf_run_name}-eks-alb-role"
-
-  assume_role_policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
-    }
-  ]
-}
-POLICY
-}
-
-resource "aws_iam_role_policy_attachment" "AmazonALBPolicyAttachment" {
-  policy_arn = aws_iam_policy.eks_alb_policy.arn
-  role       = aws_iam_role.eks_alb_role.name
-}
